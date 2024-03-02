8f6649b94df1b84bc1478dc56416f313cbf59f89
Claudia.Alvaro <claudia.alvaro17@gmail.com>
2024-03-02 12:32:13 +0100

    feat: CRUD services complete

--- src/controllers/serviceController.ts
+++ src/controllers/serviceController.ts
@@ -73,3 +73,89 @@ export const getServices = async (req: Request, res: Response) => {
     }
 
 }
+// DELETE SERVICES--------------------------------
+
+export const deleteServiceById = async (req: Request, res: Response) => {
+    // / recuperar info por parámetro
+    try {
+        const serviceId = req.params.id
+
+        const serviceToRemove: any = await Service.findOneBy({
+            id: parseInt(serviceId)
+        })
+        // validar info
+
+        if (!serviceToRemove) {
+            res.status(404).json({
+                success: false,
+                message: "service can't be deleted"
+            })
+        }
+
+        const serviceDeleted = await Service.delete(serviceToRemove)
+        res.status(200).json({
+            success: true,
+            messagge: "service deleted",
+            data: serviceDeleted
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            messagge: "service can't be  deleted",
+            error: error
+
+        })
+
+    }
+}
+
+// UPDATE SERVICES--------------------------------
+export const updateServiceById = async (req: Request, res: Response) => {
+    // / recuperar info por parámetro
+    try {
+        const serviceId = req.params.id
+        const serviceName= req.body.serviceName
+        const description=req.body.description
+        console.log(description);
+        
+
+        const serviceToUpdate: any = await Service.findOneBy({
+            id: parseInt(serviceId)
+        })
+        // validar info
+
+        if (!serviceToUpdate) {
+            res.status(404).json({
+                success: false,
+                message: "service can't be updated"
+            })
+        }
+        // actualizar datos en bd
+        const serviceUpdated = await Service.update(
+         
+            {
+                id: parseInt(serviceId)
+            },
+            {
+                serviceName: serviceName,
+                description: description
+            },
+
+        )
+        res.status(200).json({
+            success: true,
+            messagge: "service updeted",
+            data: serviceUpdated
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            messagge: "service can't be  updated",
+            error: error
+
+        })
+
+    }
+}
