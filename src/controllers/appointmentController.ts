87b0da06805e67aa8a3e90994c5b9d3ba3b4dc3f
klauha <claudiaalvaro17@gmail.com>
2024-03-04 23:03:41 +0100

    update appoinments

--- src/controllers/appointmentController.ts
+++ src/controllers/appointmentController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import { Appoinment } from "../models/Appoinment"
+import { app } from "../app"
 
 // CREATE APPOINMENT--------------------------
 
@@ -7,16 +8,16 @@ export const createAppoinment = async (req: Request, res: Response) => {
     // Recuperar la info
     try {
         const userId = req.tokenData.userId
-        const serviceId= req.body.serviceId
+        const serviceId = req.body.serviceId
         const appoinmentDate = req.body.appoinmentDate
 
-        if(!serviceId || !appoinmentDate){
+        if (!serviceId || !appoinmentDate) {
             return res.status(400).json({
                 success: false,
                 message: "Service ID and date are needed",
             })
         }
-        const newAppoinment= await Appoinment.create(
+        const newAppoinment = await Appoinment.create(
             {
                 appointmentDate: appoinmentDate,
                 user: {
@@ -34,51 +35,69 @@ export const createAppoinment = async (req: Request, res: Response) => {
             data: newAppoinment
         })
 
-    }catch(error:any){
+    } catch (error: any) {
         res.status(500).json({
-        success: false,
-        message: "Appoinment can't be created",
-        error: error.message
-    })}
+            success: false,
+            message: "Appoinment can't be created",
+            error: error.message
+        })
+    }
 }
 
 // RECUPERAR 1 CITA:
 
 
-// export const getAppointmentById = async (req: Request, res: Response) => {
-//     try {
-//         const appoinmentId = req.params.appoinmentId
+export const getAppointmentById = async (req: Request, res: Response) => {
+    try {
+        const userId = req.tokenData.userId
+        const appoinmentId = req.params.id
 
-//         const appoinmentFound = await Appoinment.findOne(
-//             // {
-//             // where: {
-//     //         //     id: appoinmentId as number
-//     //         // }
-//     // })
+        const appoinmentFound = await Appoinment.findOne(
+            {
+                where: {
+                    id: parseInt(appoinmentId),
+                    user: {
+                        id: userId
+                    }
+                },
+                relations: {
+                    service: true
+                }
+            }
+        )
 
-//         res.status(200).json({
-//             suceess: true,
-//             message: "Appointmen retrieved successfully",
-//             data: appoinmentFound
-//         })
-        
-//     } catch (error) {
-//         res.status(500).json({
-//             success: false,
-//             message: "services cant be retrieved",
-//             error: error
-//         })
-//     }
+        if (!appoinmentFound) {
+            res.status(404).json(
+                {
+                    success: false,
+                    message: "appoinment not found"
+                }
+            )
+        }
 
-// }
+        res.status(200).json({
+            suceess: true,
+            message: "Appointmen retrieved successfully",
+            data: appoinmentFound
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Appoinment cant be retrieved",
+            error: error
+        })
+    }
+
+}
 
 // RECUPERAR MIS CITAS
 
-export const getMyAppoinments = async (req:Request, res:Response)=> {
+export const getMyAppoinments = async (req: Request, res: Response) => {
     console.log(req.tokenData);
-    
-    try{
-        const userId= req.tokenData.userId
+
+    try {
+        const userId = req.tokenData.userId
 
         const myAppointments = await Appoinment.find({
             where: {
@@ -99,22 +118,67 @@ export const getMyAppoinments = async (req:Request, res:Response)=> {
                 data: myAppointments
             }
         )
-        
-        
 
-    } catch (error){
-        res.status(500).json ({
-            success:false,
-            message:"apointments can't be retrieved",
+
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "apointments can't be retrieved",
         }
-    )}
+        )
+    }
 }
 
+// MODIFICAR CITA
 
-// export const updateAppoinment = async (req:Request, res:Response)=> {
-//     const selectedAppoinment =await Appoinment.findOne({
-//         where: {id:appoinmentId},
-//     }
+export const updateAppoinmentById = async (req: Request, res: Response) => {
+    try {
+        const appoinmentId = req.params.id
+        const serviceId = req.body.serviceId
+        const appoinmentDate = req.body.appoinmentDate
 
-//     )
-// }
+        const appoinmentToUpdate = await Appoinment.findOne(
+            {
+                where: {
+                    id: parseInt(appoinmentId),
+                    user: {
+                        id: req.tokenData.userId
+                    }
+                }
+            }
+        )
+
+        if(!appoinmentToUpdate) {
+            return res.status(404).json({
+                success: false,
+                messagge: "appointment not found",
+            })
+        }
+
+        const appointmentUpdated = await Appoinment.update(
+            {
+                id: parseInt(appoinmentId)
+            },
+            {
+                service: {
+                    id: parseInt(serviceId)
+                },
+                appointmentDate: appoinmentDate
+            }
+        )
+        res.status(200).json({
+            success: true,
+            messagge: "service updeted",
+            data: appointmentUpdated
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            messagge: "Appoinment can't be  updated",
+            error: error
+        })
+
+    }
+}
